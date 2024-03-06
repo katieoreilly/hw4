7cdb4b2be4cf4184927968725bb3073e7b2ab612
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-03-06 23:11:13 +0000

    authorization complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["username"] = params["username"]
-    @user["email"] = params["email"]
-    @user["password"] = BCrypt::Password.create(params["password"])
-    @user.save
-    flash[:notice] = "Thanks for signing up. Now login."
-    redirect_to "/places"
+    if User.find_by({ "username" => params["username"] }) == nil
+      @user = User.new
+      @user["username"] = params["username"]
+      @user["email"] = params["email"]
+      @user["password"] = BCrypt::Password.create(params["password"])
+      @user.save
+      flash[:notice] = "Thanks for signing up. Now login."
+      redirect_to "/login"
+    else
+      flash["notice"] = "Username taken."
+      redirect_to "/users/new"
+    end
   end
-end
+end   

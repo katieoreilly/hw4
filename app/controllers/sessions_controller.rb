7cbea2fdbcda51876629c02182e6dbcb0e004ed0
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-03-07 23:48:55 +0000

    authorization almost done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,14 @@ class SessionsController < ApplicationController
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Hello."
+        flash["notice"] = "Hello! You are now logged-in."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
+        flash["notice"] = "Sorry, invalid username or password."
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Nope."
+      flash["notice"] = "Sorry, invalid username or password."
       redirect_to "/login"
     end
   end
